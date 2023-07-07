927673f57d65fa4a8515970d6dcd1c3f85e431c6
Liz Erlandson <elizabeth.erlandson1@gmail.com>
2023-07-06 23:06:44 -0400

    first push of flask-blogly part 2

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 """Models for Blogly."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -24,3 +24,35 @@ class user(db.Model):
                      nullable=False,
                      unique=True)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+    
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
+    @property
+    def full_name(self):
+
+        return f"{self.first_name} {self.last_name}"
+class POST(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.Text,
+                     nullable=False,
+                     unique=True)
+    content = db.Column(db.Text,
+                     nullable=False,
+                     unique=True)
+created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+user_id = db.Column(db. Integer, db.ForeignKey('users.id'), nullable=False)
+
+@property
+    def friendly_date(self):
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+def connect_db(app):
+
+    db.app = app
+    db.init_app(app)
